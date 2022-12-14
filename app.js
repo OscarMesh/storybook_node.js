e365dfcf39175e0dc1b5282931a4a4642e3d7b0e
osacrMesh <okerekeifeanyi65@gmail.com>
2022-12-14 08:53:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const connectDB = require("./config/db");
+
+// load config
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars
+app.engine(".hbs", engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/index"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.Node_ENV} mode on port ${PORT} `)
+);
